ff3466094215f0bbc16b2f5bb5852ead4c2c43d4
draftcode <draftcode@gmail.com>
2013-10-26 13:02:53 +0900

    [from now] 2013/10/26 13:02:53

---
+++ scanner.go
@@ -0,0 +1,413 @@
+package sandal
+
+import (
+	"unicode"
+)
+
+const (
+	EOF        = -1
+	UNKNOWN    = 0
+	IDENTIFIER = 30000 + iota
+	NUMBER
+	COMMENT
+
+	ADD // +
+	SUB // -
+	MUL // *
+	QUO // /
+	REM // %
+
+	AND // &
+	OR  // |
+	XOR // ^
+	SHL // <<
+	SHR // >>
+
+	ADD_ASSIGN // +=
+	SUB_ASSIGN // -=
+	MUL_ASSIGN // *=
+	QUO_ASSIGN // /=
+	REM_ASSIGN // %=
+
+	AND_ASSIGN // &=
+	OR_ASSIGN  // |=
+	XOR_ASSIGN // ^=
+	SHL_ASSIGN // <<=
+	SHR_ASSIGN // >>=
+
+	LAND // &&
+	LOR  // ||
+
+	EQL    // ==
+	LSS    // <
+	GTR    // >
+	ASSIGN // =
+	NOT    // !
+
+	NEQ // !=
+	LEQ // <=
+	GEQ // >=
+
+	DATA
+	CONST
+	MODULE
+	CHANNEL
+	BIT
+	BOOL
+	BYTE
+	PROC
+	VAR
+	IF
+	ELSE
+	CHOICE
+	RECV
+	TIMEOUT_RECV
+	PEEK
+	TIMEOUT_PEEK
+	SEND
+	FOR
+	BREAK
+	IN
+	RANGE
+	TO
+	INIT
+	GOTO
+	UNSTABLE
+	SKIP
+)
+
+var keywords = map[string]int{
+	"data":         DATA,
+	"const":        CONST,
+	"module":       MODULE,
+	"channel":      CHANNEL,
+	"bit":          BIT,
+	"bool":         BOOL,
+	"byte":         BYTE,
+	"proc":         PROC,
+	"var":          VAR,
+	"if":           IF,
+	"else":         ELSE,
+	"choice":       CHOICE,
+	"recv":         RECV,
+	"timeout_recv": TIMEOUT_RECV,
+	"peek":         PEEK,
+	"timeout_peek": TIMEOUT_PEEK,
+	"send":         SEND,
+	"for":          FOR,
+	"break":        BREAK,
+	"in":           IN,
+	"range":        RANGE,
+	"to":           TO,
+	"init":         INIT,
+	"goto":         GOTO,
+	"unstable":     UNSTABLE,
+	"skip":         SKIP,
+}
+
+// Position indicates a position of a first character of a token in a file.
+type Position struct {
+	Line   int
+	Column int
+}
+
+type Mode uint
+
+const (
+	dontInsertSemis = 1 << iota
+)
+
+// Scanner is a lexer on Sandal language.
+type Scanner struct {
+	src       []rune
+	offset    int
+	lineHead  int
+	line      int
+	mode      Mode
+	lastToken int
+}
+
+func (s *Scanner) Init(src []rune, mode Mode) {
+	s.src = src
+	s.mode = mode
+}
+
+// Scan returns token and literal and its position. Return UNKNOWN if unknown
+// token received. Return EOF after all characters consumed.
+func (s *Scanner) Scan() (tok int, lit string, pos Position) {
+	if s.skipWhiteSpace() && (s.mode & dontInsertSemis) == 0 {
+		switch s.lastToken {
+		case IDENTIFIER, NUMBER, BREAK, ')', ']', '}':
+			s.lastToken = int(';')
+			tok = int(';')
+			lit = "\n"
+			return
+		}
+	}
+	savedOffset := s.offset // Used in COMMENT
+	pos = s.position()
+	switch ch := s.peek(); {
+	case isLetter(ch):
+		lit = s.scanIdentifier()
+		if keyword, ok := keywords[lit]; ok {
+			tok = keyword
+		} else {
+			tok = IDENTIFIER
+		}
+	case isDigit(ch):
+		tok, lit = NUMBER, s.scanNumber()
+	default:
+		s.next()
+		switch ch {
+		case -1:
+			tok = EOF
+		case '{', '}', '(', ')', '[', ']', ',', ':', ';':
+			tok = int(ch)
+			lit = string(ch)
+		case '+':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = ADD_ASSIGN
+				lit = "+="
+			default:
+				tok = ADD
+				lit = "+"
+			}
+		case '-':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = SUB_ASSIGN
+				lit = "-="
+			default:
+				tok = SUB
+				lit = "-"
+			}
+		case '*':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = MUL_ASSIGN
+				lit = "*="
+			default:
+				tok = MUL
+				lit = "*"
+			}
+		case '/':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = QUO_ASSIGN
+				lit = "/="
+			case '/':
+				// Insert semicolon before comment.
+				switch s.lastToken {
+				case IDENTIFIER, NUMBER, BREAK, ')', ']', '}':
+					s.offset = savedOffset
+					tok = int(';')
+					lit = "\n"
+				default:
+					s.next()
+					tok = COMMENT
+					lit = "//" + s.scanLineComment()
+				}
+			default:
+				tok = QUO
+				lit = "/"
+			}
+		case '%':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = REM_ASSIGN
+				lit = "%="
+			default:
+				tok = REM
+				lit = "%"
+			}
+		case '&':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = AND_ASSIGN
+				lit = "&="
+			case '&':
+				s.next()
+				tok = LAND
+				lit = "&&"
+			default:
+				tok = AND
+				lit = "&"
+			}
+		case '|':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = OR_ASSIGN
+				lit = "|="
+			case '|':
+				s.next()
+				tok = LOR
+				lit = "||"
+			default:
+				tok = OR
+				lit = "|"
+			}
+		case '^':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = XOR_ASSIGN
+				lit = "^="
+			default:
+				tok = XOR
+				lit = "^"
+			}
+		case '<':
+			switch s.peek() {
+			case '<':
+				s.next()
+				switch s.peek() {
+				case '=':
+					s.next()
+					tok = SHL_ASSIGN
+					lit = "<<="
+				default:
+					tok = SHL
+					lit = "<<"
+				}
+			case '=':
+				s.next()
+				tok = LEQ
+				lit = "<="
+			default:
+				tok = LSS
+				lit = "<"
+			}
+		case '>':
+			switch s.peek() {
+			case '>':
+				s.next()
+				switch s.peek() {
+				case '=':
+					s.next()
+					tok = SHR_ASSIGN
+					lit = ">>="
+				default:
+					tok = SHR
+					lit = ">>"
+				}
+			case '=':
+				s.next()
+				tok = GEQ
+				lit = ">="
+			default:
+				tok = GTR
+				lit = ">"
+			}
+		case '=':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = EQL
+				lit = "=="
+			default:
+				tok = ASSIGN
+				lit = "="
+			}
+		case '!':
+			switch s.peek() {
+			case '=':
+				s.next()
+				tok = NEQ
+				lit = "!="
+			default:
+				tok = NOT
+				lit = "!"
+			}
+		}
+	}
+	s.lastToken = tok
+	return
+}
+
+// ========================================
+
+func isLetter(ch rune) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' || ch >= 0x80 && unicode.IsLetter(ch)
+}
+
+func isDigit(ch rune) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func isWhiteSpace(ch rune) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n'
+}
+
+func (s *Scanner) reachEOF() bool {
+	return len(s.src) <= s.offset
+}
+
+func (s *Scanner) peek() rune {
+	if !s.reachEOF() {
+		return s.src[s.offset]
+	} else {
+		return -1
+	}
+}
+
+func (s *Scanner) next() {
+	if !s.reachEOF() {
+		if s.peek() == '\n' {
+			s.lineHead = s.offset + 1
+			s.line++
+		}
+		s.offset++
+	}
+}
+
+func (s *Scanner) position() Position {
+	return Position{Line: s.line + 1, Column: s.offset - s.lineHead + 1}
+}
+
+func (s *Scanner) skipWhiteSpace() (includeReturn bool) {
+	for isWhiteSpace(s.peek()) {
+		if s.peek() == '\n' {
+			includeReturn = true
+		}
+		s.next()
+	}
+	if s.reachEOF() {
+		includeReturn = true
+	}
+	return
+}
+
+func (s *Scanner) scanIdentifier() string {
+	var ret []rune
+	for isLetter(s.peek()) || isDigit(s.peek()) {
+		ret = append(ret, s.peek())
+		s.next()
+	}
+	return string(ret)
+}
+
+func (s *Scanner) scanNumber() string {
+	var ret []rune
+	for isDigit(s.peek()) {
+		ret = append(ret, s.peek())
+		s.next()
+	}
+	return string(ret)
+}
+
+func (s *Scanner) scanLineComment() string {
+	var ret []rune
+	for s.peek() != '\n' && s.peek() != -1 {
+		ret = append(ret, s.peek())
+		s.next()
+	}
+	return string(ret)
+}
---
+++ scanner_test.go
@@ -0,0 +1,165 @@
+package sandal
+
+import (
+	"testing"
+)
+
+func testScanner(t *testing.T, src string, expectTok int, expectLit string) {
+	s := new(Scanner)
+	s.Init([]rune(src), dontInsertSemis)
+	tok, lit, _ := s.Scan()
+	if tok != expectTok {
+		t.Errorf("Scanner{Src: \"%v\"}.Scan() = %v, _ want %v", src, tok, expectTok)
+	}
+	if lit != expectLit {
+		t.Errorf("Scanner{Src: \"%v\"}.Scan() = _, %v want %v", src, lit, expectLit)
+	}
+	tok, lit, _ = s.Scan()
+	if tok != EOF {
+		t.Errorf("Scanner{Src: \"%v\"}.Scan() = %v, _ want %v", src, tok, EOF)
+	}
+}
+
+func TestScanSymbols(t *testing.T) {
+	testScanner(t, "{", '{', "{")
+	testScanner(t, "}", '}', "}")
+	testScanner(t, "(", '(', "(")
+	testScanner(t, ")", ')', ")")
+	testScanner(t, "[", '[', "[")
+	testScanner(t, "]", ']', "]")
+	testScanner(t, ",", ',', ",")
+	testScanner(t, ":", ':', ":")
+	testScanner(t, ";", ';', ";")
+
+	testScanner(t, "+", ADD, "+")
+	testScanner(t, "-", SUB, "-")
+	testScanner(t, "*", MUL, "*")
+	testScanner(t, "/", QUO, "/")
+	testScanner(t, "%", REM, "%")
+
+	testScanner(t, "&", AND, "&")
+	testScanner(t, "|", OR, "|")
+	testScanner(t, "^", XOR, "^")
+	testScanner(t, "<<", SHL, "<<")
+	testScanner(t, ">>", SHR, ">>")
+
+	testScanner(t, "+=", ADD_ASSIGN, "+=")
+	testScanner(t, "-=", SUB_ASSIGN, "-=")
+	testScanner(t, "*=", MUL_ASSIGN, "*=")
+	testScanner(t, "/=", QUO_ASSIGN, "/=")
+	testScanner(t, "%=", REM_ASSIGN, "%=")
+
+	testScanner(t, "&=", AND_ASSIGN, "&=")
+	testScanner(t, "|=", OR_ASSIGN, "|=")
+	testScanner(t, "^=", XOR_ASSIGN, "^=")
+	testScanner(t, "<<=", SHL_ASSIGN, "<<=")
+	testScanner(t, ">>=", SHR_ASSIGN, ">>=")
+
+	testScanner(t, "&&", LAND, "&&")
+	testScanner(t, "||", LOR, "||")
+
+	testScanner(t, "==", EQL, "==")
+	testScanner(t, "<", LSS, "<")
+	testScanner(t, ">", GTR, ">")
+	testScanner(t, "=", ASSIGN, "=")
+	testScanner(t, "!", NOT, "!")
+
+	testScanner(t, "!=", NEQ, "!=")
+	testScanner(t, "<=", LEQ, "<=")
+	testScanner(t, ">=", GEQ, ">=")
+}
+
+func TestScanIdentifier(t *testing.T) {
+	testScanner(t, "Sample", IDENTIFIER, "Sample")
+}
+
+func TestScanNumber(t *testing.T) {
+	testScanner(t, "0", NUMBER, "0")
+	testScanner(t, "1", NUMBER, "1")
+	testScanner(t, "119", NUMBER, "119")
+}
+
+func TestScanKeyword(t *testing.T) {
+	testScanner(t, "data", DATA, "data")
+	testScanner(t, "const", CONST, "const")
+	testScanner(t, "module", MODULE, "module")
+	testScanner(t, "channel", CHANNEL, "channel")
+	testScanner(t, "bit", BIT, "bit")
+	testScanner(t, "bool", BOOL, "bool")
+	testScanner(t, "byte", BYTE, "byte")
+	testScanner(t, "proc", PROC, "proc")
+	testScanner(t, "var", VAR, "var")
+	testScanner(t, "if", IF, "if")
+	testScanner(t, "else", ELSE, "else")
+	testScanner(t, "choice", CHOICE, "choice")
+	testScanner(t, "recv", RECV, "recv")
+	testScanner(t, "timeout_recv", TIMEOUT_RECV, "timeout_recv")
+	testScanner(t, "peek", PEEK, "peek")
+	testScanner(t, "timeout_peek", TIMEOUT_PEEK, "timeout_peek")
+	testScanner(t, "send", SEND, "send")
+	testScanner(t, "for", FOR, "for")
+	testScanner(t, "break", BREAK, "break")
+	testScanner(t, "in", IN, "in")
+	testScanner(t, "range", RANGE, "range")
+	testScanner(t, "to", TO, "to")
+	testScanner(t, "init", INIT, "init")
+	testScanner(t, "goto", GOTO, "goto")
+	testScanner(t, "unstable", UNSTABLE, "unstable")
+	testScanner(t, "skip", SKIP, "skip")
+}
+
+func TestScanComment(t *testing.T) {
+	testScanner(t, "// This is a comment", COMMENT, "// This is a comment")
+}
+
+func TestEOF(t *testing.T) {
+	s := new(Scanner)
+	s.Init([]rune(""), 0)
+	for i := 0; i < 5; i++ {
+		tok, lit, pos := s.Scan()
+		if tok != EOF || lit != "" {
+			t.Errorf("Expect EOF and \"\" but got %v and %v.", tok, lit)
+		}
+		if pos.Line != 1 || pos.Column != 1 {
+			t.Errorf("Expect Pos{1, 1} but got %v.", pos)
+		}
+	}
+}
+
+func TestInsertSemi(t *testing.T) {
+	{
+		s := new(Scanner)
+		s.Init([]rune("token\n"), 0)
+		s.Scan()
+		tok, lit, _ := s.Scan()
+		if tok != ';' || lit != "\n" {
+			t.Errorf("Expect ; and \"\\n\" but got %v and %v.", tok, lit)
+		}
+	}
+	{
+		s := new(Scanner)
+		s.Init([]rune("token"), 0)
+		s.Scan()
+		tok, lit, _ := s.Scan()
+		if tok != ';' || lit != "\n" {
+			t.Errorf("Expect ; and \"\\n\" but got %v and %v.", tok, lit)
+		}
+	}
+	{
+		s := new(Scanner)
+		s.Init([]rune("token // comment"), 0)
+		var tok int
+		if tok, _, _ = s.Scan(); tok != IDENTIFIER {
+			t.Errorf("Expect %v but got %v", IDENTIFIER, tok)
+		}
+		if tok, _, _ = s.Scan(); tok != ';' {
+			t.Errorf("Expect %v but got %v", ';', tok)
+		}
+		if tok, _, _ = s.Scan(); tok != COMMENT {
+			t.Errorf("Expect %v but got %v", COMMENT, tok)
+		}
+		if tok, _, _ = s.Scan(); tok != EOF {
+			t.Errorf("Expect %v but got %v", EOF, tok)
+		}
+	}
+}
